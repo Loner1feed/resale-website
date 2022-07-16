83c21c00780f31b0ca732fd0d8cb03d075f0a5b2
Eugen Guriev <gto200313@gmail.com>
2022-07-16 21:57:17 +0300

    added catalog page and product page

--- js/index.js
+++ js/index.js
@@ -1,23 +1,129 @@
 
 
-$('.header__toggler').click(function(){
-  $(this).toggleClass('header__toggler--active');
-  $('.header__nav').toggleClass('header__nav--open');
-})
+$(document).ready(function() {
 
-
-$('#product-slider-1').slick({
-  slidesToShow: 2,
-  sliderToScroll: 1,
-  arrows: true,
-  prevArrow: '<a href="javascript://" class="arrow arrow--prev"><span class="icon-chevron"></span></a>',
-  nextArrow: '<a href="javascript://" class="arrow arrow--next"><span class="icon-chevron"></span></a>',
-  responsive: [
-    {
-      breakpoint: 620,
-      settings: {
-        slidesToShow: 1,
+  $('.header__toggler').click(function(){
+    $(this).toggleClass('header__toggler--active');
+    $('body').toggleClass('locked');
+    $('.header__nav').toggleClass('header__nav--open');
+  })
+  
+  $('#product-slider-1').slick({
+    slidesToShow: 2,
+    sliderToScroll: 1,
+    arrows: true,
+    prevArrow: '<a href="javascript://" class="arrow arrow--prev"><span class="icon-chevron"></span></a>',
+    nextArrow: '<a href="javascript://" class="arrow arrow--next"><span class="icon-chevron"></span></a>',
+    responsive: [
+      {
+        breakpoint: 620,
+        settings: {
+          slidesToShow: 1,
+        }
       }
+    ]
+  })
+
+
+  // colorr change logics
+  let colorItem = $('.colorBlock__item')
+
+  colorItem.each((index, el) => {
+    if($(el).attr('class').includes('colorBlock__item--active')) {
+      console.log('yes')
+      let attr = $(el).attr('data-title');
+      $('.colorBlock__heading span').append(attr)
     }
-  ]
+  });
+
+  colorItem.click(function() {
+    if(!$(this).attr('class').includes('colorBlock__item--active')) {
+      colorItem.removeClass('colorBlock__item--active');
+      $(this).addClass('colorBlock__item--active');
+      let attr = $(this).attr('data-title');
+      let currentHeading = $('.colorBlock__heading span').text();
+      $('.colorBlock__heading span').text($(".colorBlock__heading span").text().replace(currentHeading, attr));
+    }
+  })
+
+  // input type number logics
+  jQuery('<div class="quantity-nav"><div class="quantity-button quantity-up"><span class="icon-chevron"><span/></div><div class="quantity-button quantity-down"><span class="icon-chevron"><span/></div></div>').insertAfter('.quantity input');
+  jQuery('.quantity').each(function() {
+    var spinner = jQuery(this),
+      input = spinner.find('input[type="number"]'),
+      btnUp = spinner.find('.quantity-up'),
+      btnDown = spinner.find('.quantity-down'),
+      min = input.attr('min'),
+      max = input.attr('max');
+
+    btnUp.click(function() {
+      var oldValue = parseFloat(input.val());
+      if (oldValue >= max) {
+        var newVal = oldValue;
+      } else {
+        var newVal = oldValue + 1;
+      }
+      spinner.find("input").val(newVal);
+      spinner.find("input").trigger("change");
+    });
+
+    btnDown.click(function() {
+      var oldValue = parseFloat(input.val());
+      if (oldValue <= min) {
+        var newVal = oldValue;
+      } else {
+        var newVal = oldValue - 1;
+      }
+      spinner.find("input").val(newVal);
+      spinner.find("input").trigger("change");
+    });
+
+  });
+
+
+  // gallery logics
+  $('.gallerySlider__for').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    fade: true,
+    asNavFor: '.gallerySlider__nav',
+    // vertical: true
+    responsive: [
+      {
+        breakpoint: 800,
+        settings: {
+          dots: true
+        }
+      }
+    ]
+  });
+  $('.gallerySlider__nav').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    asNavFor: '.gallerySlider__for',
+    centerMode: true,
+    focusOnSelect: true,
+    vertical: true,
+  });
+
+
+  // product description logics 
+
+  // $(window).resize(function() {
+  //   if($(window).width() > '620') {
+  //     $('.productDesc section p').each((index, el) => {
+  //       if(el.attr('style') == 'display: none;') {
+  //         $(el).slideDown();
+  //       }
+  //     })
+  //   }
+  // })
+
+  $('.productDesc section h5').click(function() {
+    if($(window).width() <= '620') {
+      $(this).siblings('p').slideToggle();
+      $(this).children('.productDesc__plus').toggleClass('productDesc__plus--minus')
+    }
+  })
 })
